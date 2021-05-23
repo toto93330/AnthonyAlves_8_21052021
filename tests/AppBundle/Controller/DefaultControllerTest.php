58b21a4b12e42ee5dd7e00a055fa44b0f5067b0e
Anthony Alves <contact@anthonyalves.fr>
2021-05-23 13:44:31 +0200

    mise en place de test pour les formulaires avec phpunit

--- tests/AppBundle/Controller/DefaultControllerTest.php
+++ tests/AppBundle/Controller/DefaultControllerTest.php
@@ -3,16 +3,22 @@
 namespace Tests\AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Response;
 
+/**
+ * Class DefaultControllerTest
+ * @package Tests\AppBundle\Controller
+ */
 class DefaultControllerTest extends WebTestCase
 {
-    public function testIndex()
+    public function testIndexAction()
     {
         $client = static::createClient();
+        $client->request('GET', '/');
+        static::assertSame(Response::HTTP_FOUND, $client->getResponse()->getStatusCode());
 
-        $crawler = $client->request('GET', '/');
-
-        $this->assertEquals(200, $client->getResponse()->getStatusCode());
-        $this->assertContains('Welcome to Symfony', $crawler->filter('#container h1')->text());
+        $crawler = $client->followRedirect();
+        static::assertSame(Response::HTTP_OK, $client->getResponse()->getStatusCode());
+        static::assertContains('/login_check', $crawler->filter('form')->attr('action'));
     }
 }
